eef96b36ab20880b2031de309eb26ceb4808a790
doomnuggets <doomnuggets@users.noreply.github.com>
2018-04-07 20:22:45 +0200

    blegh

--- scrapebin/pastebin.py
+++ scrapebin/pastebin.py
@@ -10,13 +10,13 @@ class Pastebin(object):
         scrape_response = requests.get(scrape_url)
         return scrape_response.json()
 
-    def fetch(self, paste_key):
+    def fetch(self, paste_id):
         """Download a paste by it's identifier (key)."""
-        paste_url = 'https://pastebin.com/api_scrape_item.php?i={}'.format(paste_key)
+        paste_url = 'https://pastebin.com/api_scrape_item.php?i={}'.format(paste_id)
         response = requests.get(paste_url)
         return response.content
 
-    def fetch_many(self, paste_keys):
-        """Fetch multiple pastes identified by their paste key."""
-        for paste_key in paste_keys:
-            yield paste_key, self.fetch(paste_key)
+    def fetch_many(self, paste_ids):
+        """Fetch multiple pastes identified by their key."""
+        for paste_id in paste_ids:
+            yield paste_id, self.fetch(paste_id)
