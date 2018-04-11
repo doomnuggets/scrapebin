1b4aebd1534618682e78393321e9f92f7881497b
doomnuggets <doomnuggets@users.noreply.github.com>
2018-04-11 10:32:37 +0200

    loooose

--- package_loose_ends.py
+++ package_loose_ends.py
@@ -5,5 +5,6 @@ from scrapebin.models import Paste
 for root, dirs, files in os.walk('pastes'):
     for f in files:
         loose_file = os.path.join(root, f)
+        p = Paste.get(f)
         target_file = os.path.join('pastes', p.human_readable_date.strftime('%Y-%m-%d'), p.id)
         os.rename(loose_file, target_file)
